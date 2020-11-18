63333ddaf642b3116aa1179874697739a8c5690b
MaratArkhipov <maratkzn2010@yandex.ru>
2020-11-18 13:48:49 +0300

    new branch

--- src/main/java/client/MainClient.java
+++ src/main/java/client/MainClient.java
@@ -3,5 +3,6 @@ package client;
 public class MainClient {
     public static void main(String[] args) {
         new Client("./src/settings/settings");
+        System.out.println("some text on branch probe");
     }
 }
